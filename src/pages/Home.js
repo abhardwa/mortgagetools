6f20cc35034a7494e186e21a1e25470b9f0adc7c
abhardwa <abhardwa@users.noreply.github.com>
2023-06-25 00:55:32 -0400

    fixedCORSbug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,7 +68,7 @@ function Home() {
                                              across 35% of all mortgage transactions nationwide.</p>
                                         <iframe loading="eager" id='optimalBlue' name='optimalBlue' src="https://www2.optimalblue.com/OBMMI/widget.php?actbg=520919&amp;inactbg=bcbec0&amp;hoverBG=520919&amp;rate=822636&amp;footer=666666&amp;graph1=520919&amp;graph2=c8ae48&amp;graph3=184742&amp;graph4=bcbec0&amp;graph6=004bf3"  
                                             data-lazy-src="https://www2.optimalblue.com/OBMMI/widget.php?actbg=520919&amp;inactbg=bcbec0&amp;hoverBG=520919&amp;rate=822636&amp;footer=666666&amp;graph1=520919&amp;graph2=c8ae48&amp;graph3=184742&amp;graph4=bcbec0&amp;graph6=004bf3"
-                                            width={1000} height={500} title='Daily Mortgage Rates' sandbox='allow-scripts' 
+                                            width={1000} height={500} title='Daily Mortgage Rates' sandbox='allow-scripts allow-same-origin' 
                                             >
                                             </iframe>
 
